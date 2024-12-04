b733fe7acbf2d96e9e55bf0729c6c4fa93aad135
vkazbanov <vkazbanov@telematika.com>
2024-12-04 18:55:30 +0200

    decompilation profiles

--- src/main/java/ru/squareland/ModDeobf.java
+++ src/main/java/ru/squareland/ModDeobf.java
@@ -11,34 +11,39 @@ import ru.squareland.mapper.Notch2SrgMapper;
 import ru.squareland.mapper.Srg2CsvMapper;
 
 import java.io.File;
+import java.io.FileReader;
+import java.util.Objects;
+import java.util.Properties;
 
 public class ModDeobf {
     public static void main(String[] programArgs) throws Exception {
-        String version = "1.4.7";
+        Properties profile = new Properties();
+        profile.load(new FileReader("profile.properties"));
+        String version = Objects.requireNonNull(profile.getProperty("version"), "missing game version");
         File versionFile = new File(version + ".jar");
         File deobfVersionFile = new File(version +  ".deobf.jar");
 
-        Notch2SrgMapper srg = new Notch2SrgMapper(version, "packaged");
+        File inputFile = new File(Objects.requireNonNull(profile.getProperty("input"), "missing input file"));
+        File outputFile = new File(Objects.requireNonNull(profile.getProperty("output"), "missing output file"));
+
+        Notch2SrgMapper srg = new Notch2SrgMapper(version, Objects.requireNonNull(profile.getProperty("side"), "missing mapping side"));
         Srg2CsvMapper csv = new Srg2CsvMapper(version);
         Deobfuscator de = new Deobfuscator(srg, csv);
 
-        if (!deobfVersionFile.exists()) {
+        if (!deobfVersionFile.exists() && versionFile.exists()) {
             de.remap(versionFile, deobfVersionFile);
         }  else {
             de.loadRelations(deobfVersionFile);
         }
 
-        boolean decompile = false;
-
-        File inputFile = new File("appeng-rv9-i.zip");
-        File outputFile = new File("appeng-rv9-i.deobf.jar");
-
         de.remap(inputFile, outputFile);
 
         System.out.println("Remapping done");
 
+        boolean decompile = Boolean.parseBoolean(Objects.requireNonNull(profile.getProperty("decompile"), "missing decompile flag"));
+
         if (decompile) {
-            File decompileDir = new File("decompile");
+            File decompileDir = new File(Objects.requireNonNull(profile.getProperty("decompileDir"), "missing decompileDir"));
             Options options = new DecompilationOptions(deobfVersionFile);
 
             DCCommonState dcCommonState = new DCCommonState(options, new ClassFileSourceWrapper(new ClassSource(outputFile, deobfVersionFile)));
@@ -46,5 +51,4 @@ public class ModDeobf {
             Main.doJar(dcCommonState, outputFile.toString(), dumperFactory);
         }
     }
-
 }
